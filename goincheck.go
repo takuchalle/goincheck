899fd993e06123c0556116a7e84ddde3cea45a4f
Takuya OHASHI <tky.ohashi@gmail.com>
2017-08-01 13:59:18 +0900

    add function to access Public API
     - get ticker
     - get trade
     - get order_books

---
+++ goincheck.go
@@ -0,0 +1,82 @@
+package goincheck
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const defalutBaseURL = "https://coincheck.com"
+
+type Client struct {
+	accessKey       string
+	secretAccessKey string
+
+	BaseURL *url.URL
+}
+
+type Ticker struct {
+	Last      float64 `json:"last"`
+	Bid       float64 `json:"bid"`
+	Ask       float64 `json:"ask"`
+	High      float64 `json:"high"`
+	Low       float64 `json:"low"`
+	Volume    float64 `json:"volume"`
+	Timestamp float64 `json:"timestamp"`
+}
+
+type Trade struct {
+	ID        int     `json:"id"`
+	Amount    string  `json:"amount"`
+	Rate      float64 `json:"rate"`
+	OrderType string  `json:"order_type"`
+	CreatedAt string  `json:"created_at"`
+}
+
+type OrderBook struct {
+	Asks [][]string `json:"asks"`
+	Bids [][]string `json:"bids"`
+}
+
+func NewClient(key, secretKey string) (*Client, error) {
+	if key == "" || secretKey == "" {
+		return &Client{}, errors.New("key is missing")
+	}
+
+	baseurl, _ := url.Parse(defalutBaseURL)
+
+	cli := &Client{accessKey: key, secretAccessKey: secretKey, BaseURL: baseurl}
+
+	return cli, nil
+}
+
+func (cli *Client) GetTicker() (ticker Ticker, err error) {
+	cli.httpGetRequest("/api/ticker", &ticker)
+
+	return ticker, nil
+}
+
+func (cli *Client) GetTrade() (trades []Trade, err error) {
+	cli.httpGetRequest("/api/trades", &trades)
+
+	return trades, nil
+}
+
+func (cli *Client) GetOrderBook() (orderbook OrderBook, err error) {
+	cli.httpGetRequest("/api/order_books", &orderbook)
+
+	return orderbook, nil
+}
+
+func (cli *Client) httpGetRequest(endpoint string, data interface{}) error {
+	resp, _ := http.Get(cli.BaseURL.String() + endpoint)
+
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	json.Unmarshal(body, data)
+
+	return nil
+}
---
+++ version.go
@@ -0,0 +1,7 @@
+package goincheck
+
+const version = "0.01"
+
+func Version() string {
+	return version
+}
